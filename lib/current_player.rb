6d4597c8780908fb4e94c18343e3618c63ccc3e4
Raul Sposito <raulsposito@gmail.com>
2019-07-11 14:18:40 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,6 +9,14 @@ def turn_count(board)
 end
 
 def current_player(board)
+  turn_count(board)
+  counter = 0
+  board.each do |box|
+    if box == "X" || box == "O"
+      counter +=1
+    end
+  end
+  return counter
   if counter % 2 == 0
     then O
     elsif counter % 2 == 1
